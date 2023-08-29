5245cbe19ab5bf22f1ad69b11ede95bf9d5cfccb
Mauricio Otalora <cmotalora@sena.edu.co>
2023-08-28 19:20:17 -0500

    Ajustes video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,11 +11,11 @@
 
     p.px-5(data-aos="fade-up") Le damos la bienvenida al componente formativo denominado “Evaluación y análisis de la huella hídrica”, el cual, hace parte del programa de formación complementaria “Medición de la huella hídrica”, para conocer un poco más sobre las temáticas a desarrollar, le extendemos la invitación a observar el siguiente video:
 
-    figure.mt-5
+    figure.mt-5 
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/eDEqqwEbCsQ" title="Video Huella Hidrica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/WTQzWcNSG8E" title="Video Huella Hidrica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
-
+ 
     
 </template>
 
